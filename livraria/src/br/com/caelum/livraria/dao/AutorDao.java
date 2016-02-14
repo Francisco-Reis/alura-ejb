7415a9c18e195fa72c05f877f721e071cb5e94dc
Francisco F. Reis <franciscofreis@gmail.com>
2016-02-14 18:30:34 -0200

    Aula 2

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -2,17 +2,39 @@ package br.com.caelum.livraria.dao;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jms.ServerSessionPool;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
+	
+	@Inject
+	private Banco banco;
 
-	private Banco banco = new Banco();
-
+	@PostConstruct
+	void aposCriacao() {
+		System.out.println("[INFO] AutorDao criado");
+	}
+	
 	public void salva(Autor autor) {
+		System.out.println("[INFO] Salvando autor: " + autor.getNome());
+		
+//		try {
+//			Thread.sleep(20000); //20 segundos
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 		
+		
 		banco.save(autor);
+		
+		System.out.println("[INFO] Salvou autor: " + autor.getNome());
 	}
 	
 	public List<Autor> todosAutores() {

fab2bbc12693961bc51146e554a51ca8021134d5
Francisco F. Reis <franciscofreis@gmail.com>
2016-02-28 22:07:06 -0300

    Commit final

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -3,47 +3,46 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.SessionBean;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.jms.ServerSessionPool;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
-	
-	@Inject
-	private Banco banco;
+
+	@PersistenceContext
+	private EntityManager manager;
 
 	@PostConstruct
 	void aposCriacao() {
 		System.out.println("[INFO] AutorDao criado");
 	}
-	
+
 	public void salva(Autor autor) {
 		System.out.println("[INFO] Salvando autor: " + autor.getNome());
-		
-//		try {
-//			Thread.sleep(20000); //20 segundos
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} 		
-		
-		banco.save(autor);
-		
+
+		// try {
+		// Thread.sleep(20000); //20 segundos
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		manager.persist(autor);
+
 		System.out.println("[INFO] Salvou autor: " + autor.getNome());
 	}
-	
+
 	public List<Autor> todosAutores() {
-		return banco.listaAutores();
+		return manager.createQuery("select a from Autor a", Autor.class)
+	            .getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
-		Autor autor = this.banco.buscaPelaId(autorId);
+		Autor autor = this.manager.find(Autor.class, autorId);
 		return autor;
 	}
-	
+
 }
